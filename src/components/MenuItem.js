d4f9a25e266aed26897024e61dde562fb91f1caa
shehreenr <shehreen.rahmann@gmail.com>
2025-02-21 20:03:10 -0600

    updated image file names

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,6 +4,7 @@ import React from 'react';
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
+
 const MenuItem = ({ title, description, price, count, incCount, decCount}) => {
 
     return (
